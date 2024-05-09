1d262135cdee1ba701250b63725386b6b9ac69e5
Jpedr0098 <joao.maurano.ismart@gmail.com>
2024-05-08 22:26:42 -0300

    atualizando projeto

--- assets/JS/cadastrar.js
+++ assets/JS/cadastrar.js
@@ -3,26 +3,29 @@ document.querySelector("#botao-cadastrar").addEventListener("click", () => {
     const form = document.querySelector("form")
 
     const tarefa = {
+        id: "id_" + new Date().getTime(),
         title: form.titulo.value,
         descricao: form.descricao.value,
         pontos: form.pontos.value,
         data: form.data.value
     }
     
-    validar(tarefa)
+    const erro = validar(tarefa)
 
-    console.log(tarefa)
+    if (erro == 0) salvar(tarefa)
 })
 
+function salvar(tarefa){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    tarefas.push(tarefa)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    window.location.href = "index.html"
+}
+
 function validar(tarefa){
-    document.querySelector("#titulo").classList.remove("is-error")
-    document.querySelector("#descricao").classList.remove("is-error")
-    document.querySelector("#pontos").classList.remove("is-error")
-    document.querySelector("#data").classList.remove("is-error")
-    document.querySelector("#titulo-erro").innerText = ""
-    document.querySelector("#descricao-erro").innerText = ""
-    document.querySelector("#pontos-erro").innerText = ""
-    document.querySelector("#data-erro").innerText = ""
+
+    limparErros()
+    let erro = 0
 
     const partesData = tarefa.data.split("-");
     const dataRetida = new Date(partesData[0], partesData[1] - 1, partesData[2]);
@@ -30,21 +33,33 @@ function validar(tarefa){
     if (tarefa.title.trim() == ""){
         document.querySelector("#titulo").classList.add("is-error")
         document.querySelector("#titulo-erro").innerText = "Titulo é obrigatório!"
-        console.log("Ta no erro irmão...")
+        erro = 1
     }
     if (tarefa.descricao.trim() == "" || tarefa.descricao.lenght < 10){
         document.querySelector("#descricao").classList.add("is-error")
         document.querySelector("#descricao-erro").innerText = "descricao deve ter no minimo 10 caracteres"
-        console.log("Ta no erro irmão...")
+        erro = 1
     }
     if (tarefa.pontos <= 0){
         document.querySelector("#pontos").classList.add("is-error")
         document.querySelector("#pontos-erro").innerText = "Os pontos devem ser maiores que zero!"
-        console.log("Ta no erro irmão...")
+        erro = 1
     }
     if(dataRetida < new Date() || dataRetida == "Invalid Date"){
         document.querySelector("#data").classList.add("is-error")
         document.querySelector("#data-erro").innerText = "Coloque uma data maior que o dia de hoje!"
-        console.log("Ta no erro irmão...")
+        erro = 1
     }
+    return erro
+}
+
+function limparErros(){
+
+    document.querySelector("#data-erro").innerText=""
+
+    document.querySelectorAll("input.is-error, textarea.is-error")
+    .forEach((input)=>{input.classList.remove("is-error")})
+
+    document.querySelectorAll(".nes-field span")
+    .forEach((span)=> span.innerText="") 
 }
---
+++ assets/JS/carregar.js
@@ -0,0 +1,32 @@
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+tarefas.forEach(tarefa => { card(tarefa)})
+
+function card(tarefa){
+
+    const dataForms = tarefa.data
+    const dataSep = dataForms.split("-")
+    const data = dataSep[1]+"/"+dataSep[2]
+
+    const cardTarefa = `
+    <div class="nes-container with-title is-centered is-nivel">
+            <p class="title">${tarefa.title}</p>
+            <p>${tarefa.descricao}</p>
+            <a href="#" class="nes-badge is-icon">
+                <span class="is-warning"><i class="nes-icon trophy is-small"></i></span>
+                <span class="is-primary">${tarefa.pontos}</span>
+            </a>
+            <a href="#" class="nes-badge">
+                <span class="is-primary">${data}</span>
+            </a>
+            <progress class="nes-progress is-success" value="0" max="100"></progress>
+            <button type="button" class="nes-btn is-primary">-</button>
+            <button onClick="apagar('${tarefa.id}')" type="button" class="nes-btn is-error">apagar</button>
+            <button type="button" class="nes-btn is-primary">+</button>
+        </div>
+    `
+    const card = document.createElement("div")
+    card.id = tarefa.id
+    card.innerHTML = cardTarefa
+    document.querySelector("#lista-de-tarefas").appendChild(card)
+}
